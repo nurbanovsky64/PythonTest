192e0cbdf2b6f3801e98df09a8cd2678abe24d72
nurbanovsky64 <nurbanovsky@gmail.com>
2022-11-07 20:36:17 -0600

    Working on retrieving an entire FC

--- FFXIVAPI_Test.py
+++ FFXIVAPI_Test.py
@@ -2,6 +2,7 @@ import urllib.request as urllib2
 import urllib.error
 import urllib.parse
 import json
+import pandas as pd
 
 #Search for an FC by name/server
 request = urllib2.Request("https://xivapi.com/freecompany/search?name=Frog+Squad&server=Goblin")
@@ -19,15 +20,25 @@ data = json.loads(urllib2.urlopen(request).read())
 
 #print(json.dumps(data, indent=2))
 
-playerName = data['FreeCompanyMembers'][0]['Name']
-playerServer = data['FreeCompanyMembers'][0]['Server']
-playerID = data['FreeCompanyMembers'][0]['ID']
+#Set up a Dataframe to hold players
+playerList = pd.DataFrame(columns = ['Name', 'Server', 'ID'])
 
-print("First Member is " + playerName + " from " + playerServer)
+#Access all members of the FC
+for x in range(len(data['FreeCompanyMembers'])):
 
-#Retrieve information for the first member on the list
-request = urllib2.Request("https://xivapi.com/character/" + str(playerID))
-request.add_header('User-Agent', '&lt;User-Agent&gt;')
-data = json.loads(urllib2.urlopen(request).read())
+    playerName = data['FreeCompanyMembers'][x]['Name']
+    playerServer = data['FreeCompanyMembers'][x]['Server']
+    playerID = data['FreeCompanyMembers'][x]['ID']
 
-print(json.dumps(data, indent=2))
+    newRow = pd.DataFrame({'Name': playerName, 'Server': playerServer, 'ID': playerID}, index = [0])
+
+    playerList = pd.concat([playerList, newRow], ignore_index = True)
+    print(playerName + ' added')
+
+    #Retrieve information for the member
+    request = urllib2.Request("https://xivapi.com/character/" + str(playerID))
+    request.add_header('User-Agent', '&lt;User-Agent&gt;')
+    playerData = json.loads(urllib2.urlopen(request).read())
+    print(json.dumps(playerData, indent=2))
+
+print(playerList)
