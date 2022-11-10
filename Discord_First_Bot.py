ddf294cef38ead67aef5a7b39ce0cd1dd2424d34
nurbanovsky64 <nurbanovsky@gmail.com>
2022-11-09 19:06:41 -0600

    Setup Inspire and Encouragement features

--- Discord_First_Bot.py
+++ Discord_First_Bot.py
@@ -1,21 +1,48 @@
 import discord
+import requests
+import json
+import random
 
+#Client setup
 intents = discord.Intents.all()
 client = discord.Client(intents = intents)
 
+#Word bank setup
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
+starter_encouragements = ["Cheer up!","Hang in there.","You are a great person / bot!"]
+
+#Retrieves an inspirational message from zenquotes and formats it for further use
+def get_quote():
+    response = json.loads(requests.get("https://zenquotes.io/api/random").text)
+    quote = response[0]['q'] + " - " + response[0]['a']
+    return quote
+
+#Bot logon
 @client.event
 async def on_ready():
     print("Logged in as a bot {0.user}".format(client))
 
+#When a message is sent in the channel
 @client.event
 async def on_message(message):
-    #print("Message from " + str(message.author) + " caught -- " + message.content)
+    #Avoid recursion by ignoring self-written messages
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        print("Message sent!")
-        await message.channel.send('Hello!')
+    msg = message.content
 
-token = ''
+    #Send a quick hello message to the channel
+    if msg.startswith('$hello'):
+        await message.channel.send("Howdy!")
+
+    #Send an inspiring quote (zenquotes) to the channel
+    if msg.startswith('$inspire'):
+        await message.channel.send(get_quote())
+
+    #If any message found has a sad word, send an encouragement to the channel
+    if any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(starter_encouragements))
+
+
+token = 'MTAzOTc1NzQwNjUxNjgwOTgwOQ.GABVrA.Gz9aydKwc9RWkpzEl8svTvlClrWNw8qtt1Ccto'
 client.run(token)
