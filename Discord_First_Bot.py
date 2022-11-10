8b71945f67eb945706dc71860ee404b07797f660
nurbanovsky64 <nurbanovsky@gmail.com>
2022-11-09 19:59:18 -0600

    Add/Delete/Display Messages, Toggle encouragements on/off

--- Discord_First_Bot.py
+++ Discord_First_Bot.py
@@ -9,13 +9,24 @@ client = discord.Client(intents = intents)
 
 #Word bank setup
 sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
-starter_encouragements = ["Cheer up!","Hang in there.","You are a great person / bot!"]
+encouragements = ["Cheer up!","Hang in there.","You are a great person / bot!"]
+encouraging = [True]
+
 
 #Retrieves an inspirational message from zenquotes and formats it for further use
 def get_quote():
     response = json.loads(requests.get("https://zenquotes.io/api/random").text)
     quote = response[0]['q'] + " - " + response[0]['a']
     return quote
+
+#Add a message to the list of encouragements
+def update_encouragements(msg):
+    encouragements.append(msg)
+
+#Remove a message from the list of encouragements
+def delete_encouragement(index):
+    if len(encouragements) > index:
+        del encouragements[index]
 
 #Bot logon
 @client.event
@@ -41,8 +52,34 @@ async def on_message(message):
 
     #If any message found has a sad word, send an encouragement to the channel
     if any(word in msg for word in sad_words):
-        await message.channel.send(random.choice(starter_encouragements))
+        if encouraging[0]:
+            await message.channel.send(random.choice(encouragements))
 
+    #Add an encouraging message to the list
+    if msg.startswith('$new'):
+        encouraging_msg = msg.split("$new ", 1)[1]
+        update_encouragements(encouraging_msg)
+        await message.channel.send("New Encouragement Added!")
+
+    #Remove an encouraging message from the list
+    if msg.startswith('$del'):
+        index = int(msg.split("$del ", 1)[1])
+        delete_encouragement(index)
+        await message.channel.send(encouragements)
+
+    #Send the current list to the chat
+    if msg.startswith('$list'):
+        await message.channel.send(encouragements)
+    
+    #Configure if the bot should be sending encouraging messages or not
+    if msg.startswith('$responding'):
+        value = msg.split("$responding ", 1)[1]
+        if value.lower() == 'true':
+            encouraging[0] = True
+            await message.channel.send("Encouragement is on!")
+        else:
+            encouraging[0] = False
+            await message.channel.send("Encouragement is off!")
 
 token = ''
 client.run(token)
